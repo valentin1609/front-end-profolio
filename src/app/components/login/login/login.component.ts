466ca3107c192a162fc1e99c708f10e5327176f5
valentin1609 <valentin160901@gmail.com>
2023-01-01 20:28:25 -0300

    add loading to login componenets and fix bug password validator

--- src/app/components/login/login/login.component.ts
+++ src/app/components/login/login/login.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loading : boolean = true;
+
+  constructor() {
+    //para darle tiempo de cargar al fondo
+    setTimeout(() => {
+      this.loading = false;
+    }, 500);
+   }
+
+  
 
   ngOnInit(): void {
+
   }
 
 }
--- src/app/components/login/new-account/new-account.component.ts
+++ src/app/components/login/new-account/new-account.component.ts
@@ -13,7 +13,17 @@ import Swal from 'sweetalert2';
 export class NewAccountComponent implements OnInit {
   form: FormGroup;
 
+  loading : boolean = true;
+
+
+
+
+
   constructor(private formBuilder: FormBuilder, private router: Router, private newUserService: NewUserService) {
+    setTimeout(() => {
+      this.loading = false;
+    }, 500);
+    
     this.form = this.formBuilder.group({
       nombre: ['', Validators.required],
       apellido: ['', Validators.required],
@@ -36,6 +46,11 @@ export class NewAccountComponent implements OnInit {
     this.form.get('password')?.valueChanges
     .subscribe(value => {
       this.passwordValue = value;
+      if(this.cpasswordValue === this.passwordValue){
+        this.matchingControl = true;
+      } else{
+        this.matchingControl = false;
+      }
     })
 
     this.form.get('cpassword')?.valueChanges
